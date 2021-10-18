f906ee0f1884c4f82b1f4c55fbc8abef21e894ab
Ihor Odainyi <odayny@users.noreply.github.com>
2021-10-18 15:55:33 +0200

    Create messageFromBinaryCode.java

---
+++ codesignal/src/messageFromBinaryCode.java
@@ -0,0 +1,42 @@
+/*
+You are taking part in an Escape Room challenge designed specifically for programmers. In your efforts to find a clue, you've found a binary code written on the wall behind a vase, and realized that it must be an encrypted message. After some thought, your first guess is that each consecutive 8 bits of the code stand for the character with the corresponding extended ASCII code.
+
+Assuming that your hunch is correct, decode the message.
+
+Example
+
+For code = "010010000110010101101100011011000110111100100001", the output should be
+messageFromBinaryCode(code) = "Hello!".
+
+The first 8 characters of the code are 01001000, which is 72 in the binary numeral system. 72 stands for H in the ASCII-table, so the first letter is H.
+Other letters can be obtained in the same manner.
+
+Input/Output
+
+[execution time limit] 3 seconds (java)
+
+[input] string code
+
+A string, the encrypted message consisting of characters '0' and '1'.
+
+Guaranteed constraints:
+0 < code.length < 800.
+
+[output] string
+
+
+*/
+String messageFromBinaryCode(String code) {
+    String result = "";
+    char sym = 0;
+    for (int i = code.length() - 1; i >= 0; i--) {
+        if (code.charAt(i) == '1') {
+            sym += Math.pow(2, 7 - (i % 8));
+        }
+        if (i % 8 == 0) {
+            result = sym + result;
+            sym = 0;
+        }
+    }
+    return result;
+}
