f580ebc92334c2628a50ab361611929609e1b524
Ihor Odainyi <odayny@users.noreply.github.com>
2022-01-10 09:28:46 +0100

    Create Solution.java

---
+++ leetcode/67. Add Binary/Solution.java
@@ -0,0 +1,58 @@
+/*Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+ 
+
+Constraints:
+
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+*/
+
+class Solution {
+    public String addBinary(String a, String b) {
+        int acc = 0;
+        int len = Math.max(a.length(), b.length());
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            int aIdx = a.length() - i - 1;
+            int bIdx = b.length() - i - 1;
+            int aChar = 0;
+            if (aIdx >= 0) {
+                aChar = a.charAt(aIdx) - '0';
+            }
+            int bChar = 0;
+            if (bIdx >= 0) {
+                bChar = b.charAt(bIdx) - '0';
+            }
+            int sum = acc + aChar + bChar;
+            if (sum == 0) {
+                acc = 0;
+                res = res.insert(0, '0');
+            } else if (sum == 1) {
+                acc = 0;
+                res = res.insert(0, '1');
+            } else if (sum == 2) {
+                acc = 1;
+                res = res.insert(0, '0');
+            } else {
+                acc = 1;
+                res = res.insert(0, '1');
+            }
+        }
+        if (acc == 1) {
+            res = res.insert(0, '1');
+        }
+        return res.toString();
+    }
+}
